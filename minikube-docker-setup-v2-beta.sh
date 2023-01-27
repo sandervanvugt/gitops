e9128ee821500f81261809d96ca59dd5a4c56d1f
sandervanvugt <mail@sandervanvugt.nl>
2023-01-27 09:58:58 +0100

    message

--- minikube-docker-setup-v2-beta.sh
+++ minikube-docker-setup-v2-beta.sh
@@ -13,6 +13,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 if [ $ARCH=x86_64 ]
 then
+	echo executing on $ARCH
 	#sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 	#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 	#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
