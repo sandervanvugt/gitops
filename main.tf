12d495ea539d05cd26689c9b576b7bb7ef046e2c
sandervanvugt <mail@sandervanvugt.nl>
2023-03-01 08:48:49 -0800

    message

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0b029b1931b347543"
   instance_type = "t3.micro"
 
   tags = {
